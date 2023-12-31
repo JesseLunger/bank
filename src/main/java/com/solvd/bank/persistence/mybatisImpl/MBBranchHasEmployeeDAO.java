a7576e5a6d566063503d82d5dd59d55f66a550b3
Jesse-Lunger <jlunger@uoregon.edu>
2023-12-30 18:53:48 -0800

    feed back fixes, bug fixes, finished test files

--- src/main/java/com/solvd/bank/persistence/mybatisImpl/MBBranchHasEmployeeDAO.java
+++ src/main/java/com/solvd/bank/persistence/mybatisImpl/MBBranchHasEmployeeDAO.java
@@ -1,41 +1,41 @@
-package com.solvd.bank.persistence.mybatisImpl;
-
-import com.solvd.bank.domain.BranchHasEmployee;
-import com.solvd.bank.domain.Staff;
-import com.solvd.bank.persistence.IBranchHasEmployeeDAO;
-
-import java.util.ArrayList;
-import java.util.List;
-
-public class MBBranchHasEmployeeDAO extends MBBaseClassDAO implements IBranchHasEmployeeDAO {
-    @Override
-    public void saveEntity(BranchHasEmployee branchHasEmployee) {
-
-    }
-
-    @Override
-    public BranchHasEmployee getEntityById(int id) {
-        return null;
-    }
-
-    @Override
-    public void updateEntity(BranchHasEmployee branchHasEmployee) {
-
-    }
-
-    @Override
-    public void removeEntityById(int id) {
-
-    }
-
-    @Override
-    public List<BranchHasEmployee> getAll() {
-        return null;
-    }
-
-
-    @Override
-    public ArrayList<Staff> getAllStaffByBranchId(int id) {
-        return null;
-    }
-}
+//package com.solvd.bank.persistence.mybatisImpl;
+//
+//import com.solvd.bank.domain.BranchHasEmployee;
+//import com.solvd.bank.domain.Staff;
+//import com.solvd.bank.persistence.IBranchHasEmployeeDAO;
+//
+//import java.util.ArrayList;
+//import java.util.List;
+//
+//public class MBBranchHasEmployeeDAO extends MBBaseClassDAO implements IBranchHasEmployeeDAO {
+//    @Override
+//    public void saveEntity(BranchHasEmployee branchHasEmployee) {
+//
+//    }
+//
+//    @Override
+//    public BranchHasEmployee getEntityById(int id) {
+//        return null;
+//    }
+//
+//    @Override
+//    public void updateEntity(BranchHasEmployee branchHasEmployee) {
+//
+//    }
+//
+//    @Override
+//    public void removeEntityById(int id) {
+//
+//    }
+//
+//    @Override
+//    public List<BranchHasEmployee> getAll() {
+//        return null;
+//    }
+//
+//
+//    @Override
+//    public ArrayList<Staff> getAllStaffByBranchId(int id) {
+//        return null;
+//    }
+//}
