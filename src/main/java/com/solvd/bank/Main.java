6b7c767d092bcbeb51f06f493b47b9590a771ad2
jesselunger <102924295+jesselunger@users.noreply.github.com>
2024-03-09 18:52:54 -0800

    adding backend schema

--- src/main/java/com/solvd/bank/Main.java
+++ src/main/java/com/solvd/bank/Main.java
@@ -9,6 +9,7 @@ import com.solvd.bank.utils.patternsutil.CountryController;
 import com.solvd.bank.utils.patternsutil.CountryView;
 import com.solvd.bank.utils.patternsutil.ExampleListeners;
 import com.solvd.bank.utils.tests.JAXBTest;
+import com.solvd.bank.utils.tests.JDBCAndMybatisTest;
 import com.solvd.bank.utils.xmlutils.JAXBUtil;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -21,43 +22,43 @@ public class Main {
 
     public static void main(String[] args) {
 
-        String filePath = System.getProperty("user.dir") + "/src/main/resources/jsonclasses/";
-
-        Country jdbcCountry = new com.solvd.bank.persistence.jdbcimpl.CountryDAO().getEntityById(1);
-        City mybatisCity = new com.solvd.bank.persistence.mybatisImpl.CityDAO().getEntityById(1);
-        Associate mybatisAssociate = new com.solvd.bank.persistence.mybatisImpl.AssociateDAO().getEntityById(1);
-
-        JAXBUtil.marshall(filePath, jdbcCountry);
-        JacksonUtil.writeJson(filePath, mybatisCity);
-        JacksonUtil.writeJson(filePath, mybatisAssociate);
-
-        //Example of Builder implementation
-        Country country = Country.builder()
-                .setId(1)
-                .setName("exampleCountry")
-                .build();
-
-        LOGGER.info(country);
-
-        //Example of Listener Implementation
-        City city = new City();
-        city.setCountry(country);
-        city.setName("exampleCity");
-        city.setId(1);
-
-        ExampleListeners exampleListeners = new ExampleListeners();
-        exampleListeners.subscribe(country);
-        exampleListeners.subscribe(city);
-        exampleListeners.notifyListeners("notifying all listeners!");
-
-        //Example of Controller Implementation
-        Country model = new CountriesService().getCountryById(1);
-        CountryView countryView = new CountryView();
-        CountryController countryController = new CountryController(model, countryView);
-        countryController.updateCountryView();
-        countryController.setCountryId(4);
-        countryController.setCountryName("changedName");
-        countryController.updateCountryView();
-        LOGGER.info(country);
+//        String filePath = System.getProperty("user.dir") + "/src/main/resources/jsonclasses/";
+//
+//        Country jdbcCountry = new com.solvd.bank.persistence.jdbcimpl.CountryDAO().getEntityById(1);
+//        City mybatisCity = new com.solvd.bank.persistence.mybatisImpl.CityDAO().getEntityById(1);
+//        Associate mybatisAssociate = new com.solvd.bank.persistence.mybatisImpl.AssociateDAO().getEntityById(1);
+//
+//        JAXBUtil.marshall(filePath, jdbcCountry);
+//        JacksonUtil.writeJson(filePath, mybatisCity);
+//        JacksonUtil.writeJson(filePath, mybatisAssociate);
+//
+//        //Example of Builder implementation
+//        Country country = Country.builder()
+//                .setId(1)
+//                .setName("exampleCountry")
+//                .build();
+//
+//        LOGGER.info(country);
+//
+//        //Example of Listener Implementation
+//        City city = new City();
+//        city.setCountry(country);
+//        city.setName("exampleCity");
+//        city.setId(1);
+//
+//        ExampleListeners exampleListeners = new ExampleListeners();
+//        exampleListeners.subscribe(country);
+//        exampleListeners.subscribe(city);
+//        exampleListeners.notifyListeners("notifying all listeners!");
+//
+//        //Example of Controller Implementation
+//        Country model = new CountriesService().getCountryById(1);
+//        CountryView countryView = new CountryView();
+//        CountryController countryController = new CountryController(model, countryView);
+//        countryController.updateCountryView();
+//        countryController.setCountryId(4);
+//        countryController.setCountryName("changedName");
+//        countryController.updateCountryView();
+//        LOGGER.info(country);
     }
 }
